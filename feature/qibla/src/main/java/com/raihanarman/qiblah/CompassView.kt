18e088ac3fbd4b625ead189eab105c94e0c95c63
raihanArman <raihanarman8@gmail.com>
2023-09-18 11:33:05 +0700

    feat: add sample compass

---
+++ feature/qibla/src/main/java/com/raihanarman/qiblah/CompassView.kt
@@ -0,0 +1,133 @@
+package com.raihanarman.qiblah
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.util.Calendar
+import java.util.Date
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+/**
+ * @author Raihan Arman
+ * @date 16/09/23
+ */
+@Composable
+fun CompassView(
+    modifier: Modifier = Modifier,
+    time: () -> Long,
+    circleRadius: Float,
+    outerCircleThickness: Float
+) {
+    var circleCenter by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+    Box(modifier = modifier) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val width = size.width
+            val height = size.height
+            circleCenter = Offset(x = width / 2f, y = height / 2f)
+            val date = Date(time())
+            val cal = Calendar.getInstance()
+            cal.time = date
+            val hours = cal.get(Calendar.HOUR_OF_DAY)
+            val minutes = cal.get(Calendar.MINUTE)
+            val seconds = cal.get(Calendar.SECOND)
+
+            drawCircle(
+                style = Stroke(
+                    width = outerCircleThickness
+                ),
+                brush = Brush.linearGradient(
+                    listOf(
+                        Color.White.copy(alpha = 0.45f),
+                        Color.DarkGray.copy(alpha = 0.35f)
+                    )
+                ),
+                radius = circleRadius + outerCircleThickness / 2f,
+                center = circleCenter
+            )
+
+            drawCircle(
+                brush = Brush.radialGradient(
+                    listOf(
+                        Color.White.copy(alpha = 0.45f),
+                        Color.DarkGray.copy(alpha = 0.25f)
+                    )
+                ),
+                radius = circleRadius,
+                center = circleCenter
+            )
+            drawCircle(
+                color = Color.Gray,
+                radius = 15f,
+                center = circleCenter
+            )
+
+            val littleLineLength = circleRadius * 0.1f
+            val largeLineLength = circleRadius * 0.2f
+
+            for (i in 0 until 60) {
+                val angelInDegrees = i * 360f / 60
+                val angelInRad = angelInDegrees * PI / 180f + PI / 2f
+                val lineLength = if (i % 5 == 0) largeLineLength else littleLineLength
+                val lineThickness = if (i % 5 == 0) 5f else 2f
+
+                val start = Offset(
+                    x = (circleRadius * cos(angelInRad) + circleCenter.x).toFloat(),
+                    y = (circleRadius * sin(angelInRad) + circleCenter.y).toFloat()
+                )
+
+                val end = Offset(
+                    x = (circleRadius * cos(angelInRad) + circleCenter.x).toFloat(),
+                    y = (circleRadius * sin(angelInRad) + lineLength + circleCenter.y).toFloat()
+                )
+
+                rotate(
+                    angelInDegrees,
+                    pivot = start
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TestCompassView() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        CompassView(
+            modifier = Modifier
+                .size(500.dp),
+            time = {
+                System.currentTimeMillis()
+            },
+            circleRadius = 600f,
+            outerCircleThickness = 50f
+        )
+    }
+}
--- feature/qibla/src/main/java/com/raihanarman/qiblah/QiblaScreen.kt
+++ feature/qibla/src/main/java/com/raihanarman/qiblah/QiblaScreen.kt
@@ -3,18 +3,10 @@ package com.raihanarman.qiblah
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
@@ -24,12 +16,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raihan.ui.bottom_sheet.BaseBottomSheet
 import com.raihanarman.qiblah.util.rememberQibla
+import com.raydev.shared.util.SharedDrawable
 
 /**
  * @author Raihan Arman
@@ -67,57 +60,67 @@ fun QiblaScreen(
     BaseBottomSheet(modifier = modifier, onDismiss = {
         onDismiss()
     }) {
-        Column(
+        Box(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            contentAlignment = Alignment.Center
         ) {
-            Box(
-                modifier = Modifier
-                    .size(200.dp)
-                    .background(Color.Gray, CircleShape)
-                    .rotate(animatedAzimuth)
-            ) {
-                Text(
-                    text = "Qibla",
-                    color = Color.Black
-                )
-                // Rotate the text based on the azimuth
-                Text(
-                    text = "🕋",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .rotate(qiblaDirection),
-                    color = Color.Black
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
+            Compass(azimuth = animatedAzimuth, qiblaDirection = qiblaDirection)
         }
     }
 }
 
 @Composable
-fun Compass(rotation: Float) {
-    Canvas(
+fun Compass(
+    azimuth: Float,
+    qiblaDirection: Float
+) {
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .size(200.dp)
+            .rotate(azimuth),
     ) {
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-
-        val centerX = canvasWidth / 2
-        val centerY = canvasHeight / 2
-        val needleLength = canvasWidth * 0.4f
-
-        // Rotate canvas based on the current rotation angle
-        rotate(rotation) {
-            drawLine(
-                color = Color.Black,
-                start = Offset(centerX, centerY),
-                end = Offset(centerX, centerY - needleLength),
-                strokeWidth = 8f.dp.toPx()
-            )
-        }
+        Image(
+            modifier = Modifier
+                .fillMaxSize(),
+            painter = painterResource(id = SharedDrawable.ic_compassface),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier
+                .fillMaxSize()
+                .rotate(qiblaDirection),
+            painter = painterResource(id = SharedDrawable.ic_compass_arrow),
+            contentDescription = null
+        )
     }
+//    Box(
+//        modifier = Modifier
+//            .size(200.dp)
+//    ) {
+//
+//        Box(
+//            modifier = Modifier
+//                .size(200.dp)
+//                .rotate(azimuth)
+//        ) {
+//            Text(
+//                text = "Qibla",
+//                color = Color.Black
+//            )
+    // Rotate the text based on the azimuth
+//            Text(
+//                text = "🕋",
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .rotate(qiblaDirection),
+//                color = Color.Black
+//            )
+//        }
+//    }
+}
+
+@Preview
+@Composable
+fun CompassPreview() {
+    Compass(0f, 0f)
 }
