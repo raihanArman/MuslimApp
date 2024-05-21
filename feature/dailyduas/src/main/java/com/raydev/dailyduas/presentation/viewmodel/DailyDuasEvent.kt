e7a3c3709c8b0eabb7310277532095249f3c3725
raihanArman <raihanarman8@gmail.com>
2024-05-21 21:34:24 +0700

    Delivers test init & does not load

---
+++ feature/dailyduas/src/main/java/com/raydev/dailyduas/presentation/viewmodel/DailyDuasEvent.kt
@@ -0,0 +1,7 @@
+package com.raydev.dailyduas.presentation.viewmodel
+
+/**
+ * @author Raihan Arman
+ * @date 21/05/24
+ */
+sealed interface DailyDuasEvent
---
+++ feature/dailyduas/src/test/java/com/raydev/dailyduas/DailyDuasViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.raydev.dailyduas
+
+import com.raydev.dailyduas.domain.GetDuasUseCase
+import com.raydev.dailyduas.presentation.viewmodel.DailyDuasState
+import io.mockk.MockKAnnotations
+import io.mockk.confirmVerified
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author Raihan Arman
+ * @date 21/05/24
+ */
+class DailyDuasViewModel {
+    private val _uiState: MutableStateFlow<DailyDuasState> = MutableStateFlow(DailyDuasState())
+    val uiState: StateFlow<DailyDuasState> = _uiState.asStateFlow()
+}
+
+class DailyDuasViewModelTest {
+    private val useCase = mockk<GetDuasUseCase>()
+    private lateinit var sut: DailyDuasViewModel
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+        sut = DailyDuasViewModel()
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @Test
+    fun testInitInitialState() {
+        val uiState = sut.uiState.value
+
+        assertFalse(uiState.isLoading)
+        assertTrue(uiState.data == null)
+        assert(uiState.errorMessage == null)
+    }
+
+    @Test
+    fun testInitDoesNotLoad() {
+        verify(exactly = 0) {
+            useCase.getDailyDuas()
+        }
+
+        confirmVerified(useCase)
+    }
+}
