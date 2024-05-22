dd8c42632932d42be2bb263e5720090b05a4288e
raihanArman <raihanarman8@gmail.com>
2024-05-22 19:05:21 +0700

    Move to production code

--- feature/dailyduas/src/test/java/com/raydev/dailyduas/DailyDuasViewModelTest.kt
+++ feature/dailyduas/src/test/java/com/raydev/dailyduas/DailyDuasViewModelTest.kt
@@ -1,14 +1,12 @@
 package com.raydev.dailyduas
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import app.cash.turbine.test
 import com.raydev.dailyduas.domain.Connectivity
 import com.raydev.dailyduas.domain.DailyDuas
 import com.raydev.dailyduas.domain.FirestoreDomainResult
 import com.raydev.dailyduas.domain.GetDuasUseCase
 import com.raydev.dailyduas.domain.Unexpected
-import com.raydev.dailyduas.presentation.viewmodel.DailyDuasState
+import com.raydev.dailyduas.presentation.viewmodel.DailyDuasViewModel
 import io.mockk.MockKAnnotations
 import io.mockk.confirmVerified
 import io.mockk.every
@@ -19,13 +17,8 @@ import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.setMain
@@ -36,55 +29,6 @@ import org.junit.Test
  * @author Raihan Arman
  * @date 21/05/24
  */
-class DailyDuasViewModel(
-    private val useCase: GetDuasUseCase
-) : ViewModel() {
-    private val _uiState: MutableStateFlow<DailyDuasState> = MutableStateFlow(DailyDuasState())
-    val uiState: StateFlow<DailyDuasState> = _uiState.asStateFlow()
-
-    fun load() {
-        viewModelScope.launch {
-            _uiState.update {
-                it.copy(
-                    isLoading = true
-                )
-            }
-            useCase.getDailyDuas().collect { result ->
-                when (result) {
-                    is FirestoreDomainResult.Failure -> {
-                        when (result.exception) {
-                            is Connectivity -> {
-                                _uiState.update {
-                                    it.copy(
-                                        errorMessage = "Tidak ada internet",
-                                        isLoading = false
-                                    )
-                                }
-                            }
-                            is Unexpected -> {
-                                _uiState.update {
-                                    it.copy(
-                                        errorMessage = "Tidak ditemukan",
-                                        isLoading = false
-                                    )
-                                }
-                            }
-                        }
-                    }
-                    is FirestoreDomainResult.Success -> {
-                        _uiState.update {
-                            it.copy(
-                                isLoading = false,
-                                data = result.root,
-                                errorMessage = null
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 
 class DailyDuasViewModelTest {
     private val useCase = mockk<GetDuasUseCase>()
