d541d773f6504dafbc690de9cf8b52a47c9cdb9f
raihanArman <raihanarman8@gmail.com>
2024-06-18 14:49:49 +0700

    delivers test load request

--- feature/dzikir/src/test/java/com/randev/dzikir/GetDzikirFirestoreUseCaseTest.kt
+++ feature/dzikir/src/test/java/com/randev/dzikir/GetDzikirFirestoreUseCaseTest.kt
@@ -1,8 +1,17 @@
 package com.randev.dzikir
 
+import app.cash.turbine.test
+import com.randev.dzikir.api.DzikirModel
+import com.randev.dzikir.domain.Dzikir
+import com.raydev.anabstract.state.FirestoreClientResult
+import com.raydev.anabstract.state.FirestoreDomainResult
 import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
@@ -10,11 +19,35 @@ import org.junit.Test
  * @author Raihan Arman
  * @date 18/06/24
  */
-interface GetDzikirFirestoreClient {
-    fun getDzikir(category: String)
+enum class DzikirCategory(value: String) {
+    PAGI("pagi"),
+    PETANG("petang")
 }
 
-class GetDzikirFirestoreUseCase
+data class DzikirRequestDto(
+    val category: DzikirCategory
+)
+
+data class DzikirRequest(
+    val category: DzikirCategory
+)
+
+interface GetDzikirFirestoreClient {
+    fun getDzikir(request: DzikirRequestDto): Flow<FirestoreClientResult<List<DzikirModel>>>
+}
+
+class GetDzikirFirestoreUseCase(
+    private val client: GetDzikirFirestoreClient
+) {
+    fun load(request: DzikirRequest): Flow<FirestoreDomainResult<List<Dzikir>>> {
+        client.getDzikir(toDtoRequest(request))
+        return flowOf()
+    }
+
+    private fun toDtoRequest(request: DzikirRequest) = DzikirRequestDto(
+        category = request.category
+    )
+}
 
 class GetDzikirFirestoreUseCaseTest {
     private val client = mockk<GetDzikirFirestoreClient>()
@@ -22,15 +55,33 @@ class GetDzikirFirestoreUseCaseTest {
 
     @Before
     fun setUp() {
-        sut = GetDzikirFirestoreUseCase()
+        sut = GetDzikirFirestoreUseCase(client)
     }
+
+    val requestDto = DzikirRequestDto(category = DzikirCategory.PAGI)
+    val request = DzikirRequest(category = DzikirCategory.PAGI)
 
     @Test
     fun testInitDoesNotLoad() {
         verify(exactly = 0) {
-            client.getDzikir(category = "")
+            client.getDzikir(requestDto)
         }
 
         confirmVerified(client)
+    }
+
+    @Test
+    fun testLoadRequestData() = runBlocking {
+        every {
+            client.getDzikir(requestDto)
+        } returns flowOf()
+
+        sut.load(request).test {
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
+            client.getDzikir(requestDto)
+        }
     }
 }
