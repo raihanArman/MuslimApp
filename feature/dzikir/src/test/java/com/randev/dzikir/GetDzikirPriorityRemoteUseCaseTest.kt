92fc912222bc4a51d03ded9ecb4c378f8c26cad3
raihanArman <raihanarman8@gmail.com>
2024-06-20 04:17:39 +0700

    delivers test fails return connectivity error

--- feature/dzikir/src/test/java/com/randev/dzikir/GetDzikirPriorityRemoteUseCaseTest.kt
+++ feature/dzikir/src/test/java/com/randev/dzikir/GetDzikirPriorityRemoteUseCaseTest.kt
@@ -3,12 +3,15 @@ package com.randev.dzikir
 import app.cash.turbine.test
 import com.randev.dzikir.api.DzikirPriorityModel
 import com.randev.dzikir.domain.DzikirPriority
+import com.raydev.anabstract.exception.Connectivity
+import com.raydev.anabstract.exception.ConnectivityException
 import com.raydev.anabstract.state.FirestoreClientResult
 import com.raydev.anabstract.state.FirestoreDomainResult
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
@@ -28,7 +31,14 @@ class GetDzikirPriorityRemoteUseCase(
     private val client: GetDzikirPriorityHttpClient
 ) {
     fun load(): Flow<FirestoreDomainResult<List<DzikirPriority>>> = flow {
-        client.getDzikirPriority()
+        client.getDzikirPriority().collect { result ->
+            when (result) {
+                is FirestoreClientResult.Failure -> {
+                    emit(FirestoreDomainResult.Failure(Connectivity()))
+                }
+                is FirestoreClientResult.Success -> {}
+            }
+        }
     }
 }
 
@@ -80,6 +90,30 @@ class GetDzikirPriorityRemoteUseCaseTest {
         }
 
         verify(exactly = 2) {
+            client.getDzikirPriority()
+        }
+
+        confirmVerified(client)
+    }
+
+    @Test
+    fun testDeliversConnectivityErrorOnClientError() = runBlocking {
+        every {
+            client.getDzikirPriority()
+        } returns flowOf(FirestoreClientResult.Failure(ConnectivityException()))
+
+        sut.load().test {
+            when (val received = awaitItem()) {
+                is FirestoreDomainResult.Failure -> {
+                    assertEquals(Connectivity()::class.java, received.exception::class.java)
+                }
+                is FirestoreDomainResult.Success -> {}
+            }
+
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
             client.getDzikirPriority()
         }
 
