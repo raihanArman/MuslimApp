2a4929420f1574c45911d2298f4a18096bfdc2fb
raihanArman <raihanarman8@gmail.com>
2024-06-29 21:33:37 +0700

    implementation dzikir firebase service

--- feature/dzikir/src/main/java/com/randev/dzikir/api_infra/DzikirFirestoreService.kt
+++ feature/dzikir/src/main/java/com/randev/dzikir/api_infra/DzikirFirestoreService.kt
@@ -1,5 +1,8 @@
 package com.randev.dzikir.api_infra
 
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 /**
  * @author Raihan Arman
  * @date 18/06/24
@@ -7,4 +10,19 @@ package com.randev.dzikir.api_infra
 interface DzikirFirestoreService {
     suspend fun getDzikir(category: String): List<DzikirResponse>
     suspend fun getDzikirPriority(): List<DzikirPriorityResponse>
+}
+
+class DzikirFirestoreServiceImpl : DzikirFirestoreService {
+    private val firestore = FirebaseFirestore.getInstance()
+    override suspend fun getDzikir(category: String): List<DzikirResponse> {
+        val snapshot = firestore.collection("dzikir").get().await()
+        val response = snapshot.toObjects(DzikirResponse::class.java)
+        return response
+    }
+
+    override suspend fun getDzikirPriority(): List<DzikirPriorityResponse> {
+        val snapshot = firestore.collection("dzikir_priority").get().await()
+        val response = snapshot.toObjects(DzikirPriorityResponse::class.java)
+        return response
+    }
 }
