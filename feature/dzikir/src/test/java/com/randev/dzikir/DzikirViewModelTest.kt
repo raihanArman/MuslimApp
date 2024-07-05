26af076a7842484320d31b7891e3859dceff85ce
raihanArman <raihanarman8@gmail.com>
2024-07-05 19:32:13 +0700

    delivers test load isloading state

--- feature/dzikir/src/test/java/com/randev/dzikir/DzikirViewModelTest.kt
+++ feature/dzikir/src/test/java/com/randev/dzikir/DzikirViewModelTest.kt
@@ -1,21 +1,29 @@
 package com.randev.dzikir
 
 import androidx.lifecycle.ViewModel
+import app.cash.turbine.test
 import com.randev.dzikir.domain.model.Dzikir
 import com.randev.dzikir.domain.request.DzikirRequest
 import com.randev.dzikir.util.DzikirCategory
 import com.raydev.anabstract.state.FirestoreDomainResult
 import io.mockk.MockKAnnotations
 import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -36,9 +44,18 @@ data class DzikirState(
     val data: List<Dzikir> ? = null,
 )
 
-class DzikirViewModel : ViewModel() {
+class DzikirViewModel(
+    private val useCase: GetDzikirUseCase
+) : ViewModel() {
     private val _uiState = MutableStateFlow(DzikirState())
     val uiState = _uiState.asStateFlow()
+
+    fun load(request: DzikirRequest) {
+        _uiState.update {
+            it.copy(isLoading = true)
+        }
+        useCase.load(request)
+    }
 }
 
 class DzikirViewModelTest {
@@ -51,7 +68,7 @@ class DzikirViewModelTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
-        sut = DzikirViewModel()
+        sut = DzikirViewModel(useCase)
         Dispatchers.setMain(UnconfinedTestDispatcher())
     }
 
@@ -71,5 +88,27 @@ class DzikirViewModelTest {
         }
 
         confirmVerified(useCase)
+    }
+
+    @Test
+    fun testLoadIsLoadingState() = runBlocking {
+        val captureRequest = slot<DzikirRequest>()
+        every {
+            useCase.load(capture(captureRequest))
+        } returns flowOf()
+
+        sut.load(request = request)
+
+        sut.uiState.take(count = 1).test {
+            val received = awaitItem()
+            assertEquals(captureRequest.captured, request)
+
+            assertTrue(received.isLoading)
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
+            useCase.load(capture(captureRequest))
+        }
     }
 }
