0271bacd5bb3c22dd08f84dbb6972b3cbeef303e
raihanArman <raihanarman8@gmail.com>
2024-07-14 12:21:26 +0700

    delivers test init state

---
+++ feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoViewModelTest.kt
@@ -0,0 +1,56 @@
+package com.raydev.shortvideo
+
+import androidx.lifecycle.ViewModel
+import com.raydev.shortvideo.domain.ShortVideo
+import io.mockk.MockKAnnotations
+import io.mockk.mockk
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author Raihan Arman
+ * @date 14/07/24
+ */
+interface GetShortVideoUseCase
+
+data class GetShortVideoState(
+    val isLoading: Boolean = false,
+    val errorMessage: String ? = null,
+    val data: List<ShortVideo> ? = null,
+)
+
+class GetShortVideoViewModel : ViewModel() {
+    private val _uiState: MutableStateFlow<GetShortVideoState> = MutableStateFlow(GetShortVideoState())
+    val uiState: StateFlow<GetShortVideoState> = _uiState.asStateFlow()
+}
+
+class GetShortVideoViewModelTest {
+    private val useCase = mockk<GetShortVideoUseCase>()
+    private lateinit var sut: GetShortVideoViewModel
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+        sut = GetShortVideoViewModel()
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @Test
+    fun testInitState() {
+        val uiState = sut.uiState.value
+
+        assertFalse(uiState.isLoading)
+        assertTrue(uiState.data == null)
+        assert(uiState.errorMessage == null)
+    }
+}
