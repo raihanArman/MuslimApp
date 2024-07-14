c89215e77510c2f3c75d6264f5c6de02ee647ebb
raihanArman <raihanarman8@gmail.com>
2024-07-14 11:13:25 +0700

    delivers test load connectivity error

--- feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoRemoteUseCaseTest.kt
+++ feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoRemoteUseCaseTest.kt
@@ -1,12 +1,15 @@
 package com.raydev.shortvideo
 
 import app.cash.turbine.test
+import com.raydev.anabstract.exception.Connectivity
+import com.raydev.anabstract.exception.ConnectivityException
 import com.raydev.anabstract.state.FirestoreClientResult
 import com.raydev.anabstract.state.FirestoreDomainResult
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
@@ -26,7 +29,19 @@ class GetShortVideoRemoteUseCase(
     private val client: GetShortVideoClient
 ) {
     fun getShortVideo(): Flow<FirestoreDomainResult<List<ShortVideo>>> = flow {
-        val result = client.getShortVideo()
+        client.getShortVideo().collect { result ->
+            when (result) {
+                is FirestoreClientResult.Failure -> {
+                    when (result.exception) {
+                        is ConnectivityException -> {
+                            emit(FirestoreDomainResult.Failure(Connectivity()))
+                        }
+                    }
+                }
+                is FirestoreClientResult.Success -> {
+                }
+            }
+        }
     }
 }
 
@@ -84,5 +99,30 @@ class GetShortVideoRemoteUseCaseTest {
         }
 
         confirmVerified(client)
+    }
+
+    @Test
+    fun testLoadDeliversConnectivityError() = runBlocking {
+        every {
+            client.getShortVideo()
+        } returns flowOf(FirestoreClientResult.Failure(ConnectivityException()))
+
+        sut.getShortVideo().test {
+            when (val received = awaitItem()) {
+                is FirestoreDomainResult.Failure -> {
+                    assertEquals(Connectivity()::class.java, received.exception::class.java)
+                }
+                is FirestoreDomainResult.Success -> {
+                }
+            }
+
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
+            client.getShortVideo()
+        }
+
+        confirmVerified()
     }
 }
