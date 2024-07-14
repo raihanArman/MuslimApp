b24a8cc0fde4b06490900d5112fd046be6db873f
raihanArman <raihanarman8@gmail.com>
2024-07-14 11:04:19 +0700

    delivers test load request data

---
+++ feature/shortvideo/src/main/java/com/raydev/shortvideo/ShortVideoModel.kt
@@ -0,0 +1,12 @@
+package com.raydev.shortvideo
+
+/**
+ * @author Raihan Arman
+ * @date 14/07/24
+ */
+data class ShortVideoModel(
+    val id: String,
+    val title: String,
+    val url: String,
+    val description: String,
+)
--- feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoRemoteUseCaseTest.kt
+++ feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoRemoteUseCaseTest.kt
@@ -1,8 +1,16 @@
 package com.raydev.shortvideo
 
+import app.cash.turbine.test
+import com.raydev.anabstract.state.FirestoreClientResult
+import com.raydev.anabstract.state.FirestoreDomainResult
 import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
@@ -11,10 +19,16 @@ import org.junit.Test
  * @date 14/07/24
  */
 interface GetShortVideoClient {
-    fun getShortVideo()
+    fun getShortVideo(): Flow<FirestoreClientResult<List<ShortVideoModel>>>
 }
 
-class GetShortVideoRemoteUseCase
+class GetShortVideoRemoteUseCase(
+    private val client: GetShortVideoClient
+) {
+    fun getShortVideo(): Flow<FirestoreDomainResult<List<ShortVideo>>> = flow {
+        val result = client.getShortVideo()
+    }
+}
 
 class GetShortVideoRemoteUseCaseTest {
     private val client = mockk<GetShortVideoClient>()
@@ -22,7 +36,7 @@ class GetShortVideoRemoteUseCaseTest {
 
     @Before
     fun setUp() {
-        sut = GetShortVideoRemoteUseCase()
+        sut = GetShortVideoRemoteUseCase(client)
     }
 
     @Test
@@ -32,5 +46,22 @@ class GetShortVideoRemoteUseCaseTest {
         }
 
         confirmVerified(client)
+    }
+
+    @Test
+    fun testLoadRequestData() = runBlocking {
+        every {
+            client.getShortVideo()
+        } returns flowOf()
+
+        sut.getShortVideo().test {
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
+            client.getShortVideo()
+        }
+
+        confirmVerified()
     }
 }
