6ef79a68d1ff32249a28558e6e19c0623a9f4c68
raihanArman <raihanarman8@gmail.com>
2024-07-14 12:29:17 +0700

    delivers test load is loading state

--- feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoViewModelTest.kt
+++ feature/shortvideo/src/test/java/com/raydev/shortvideo/GetShortVideoViewModelTest.kt
@@ -2,6 +2,7 @@ package com.raydev.shortvideo
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import app.cash.turbine.test
 import com.raydev.anabstract.state.FirestoreDomainResult
 import com.raydev.shortvideo.domain.ShortVideo
 import io.mockk.MockKAnnotations
@@ -18,7 +19,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.setMain
 import org.junit.Before
@@ -46,6 +50,11 @@ class GetShortVideoViewModel(
 
     fun load() {
         viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
             useCase.getShortVideo()
         }
     }
@@ -106,6 +115,27 @@ class GetShortVideoViewModelTest {
         sut.load()
 
         verify(exactly = 2) {
+            useCase.getShortVideo()
+        }
+
+        confirmVerified(useCase)
+    }
+
+    @Test
+    fun testLoadIsLoadingState() = runBlocking {
+        every {
+            useCase.getShortVideo()
+        } returns flowOf()
+
+        sut.load()
+
+        sut.uiState.take(count = 1).test {
+            val received = awaitItem()
+            assertTrue(received.isLoading)
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
             useCase.getShortVideo()
         }
 
