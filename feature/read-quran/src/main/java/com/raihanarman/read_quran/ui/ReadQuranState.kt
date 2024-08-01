bbdaa12280ea6dac874bc028687f2acfba6cb828
raihanArman <raihanarman8@gmail.com>
2024-08-01 17:10:33 +0700

    make listSurah to immutable list

--- feature/read-quran/src/main/java/com/raihanarman/read_quran/ui/ReadQuranState.kt
+++ feature/read-quran/src/main/java/com/raihanarman/read_quran/ui/ReadQuranState.kt
@@ -2,6 +2,7 @@ package com.raihanarman.read_quran.ui
 
 import com.raydev.shared.model.Ayah
 import com.raydev.shared.model.Surah
+import kotlinx.collections.immutable.ImmutableList
 
 /**
  * @author Raihan Arman
@@ -12,7 +13,7 @@ data class ReadQuranState(
     val tabsSelected: Int ? = null,
     val surahId: Int ? = null,
     val listAyah: List<Ayah> ? = null,
-    val listSurah: List<Surah> ? = null,
+    val listSurah: ImmutableList<Surah> ? = null,
     val bottomSheetIsOpen: Boolean = false,
     val ayahIndexSelected: Int = -1,
     val ayahSelected: Ayah ? = null,
